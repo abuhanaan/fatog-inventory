a920b8318691c9905c891a8210201579c5a30f26
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-02-25 15:05:16 +0100

    feat: Render new layout components

--- fatog-fe/src/App.jsx
+++ fatog-fe/src/App.jsx
@@ -1,22 +1,25 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import Layout from "./components/Layout";
-import Home from "./components/Home";
+import Layout from "./components/layouts/Layout";
+import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import { Route, createBrowserRouter, createRoutesFromChildren, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import RequireAuth from "./components/RequireAuth";
+import InventoryLayout from "./components/layouts/InventoryLayout";
 
 const router = createBrowserRouter(createRoutesFromChildren(
-        <Route path='/' element={<Layout />}>
-            <Route index element={<Home />} />
+    <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
 
-            <Route element={<RequireAuth />}>
+        <Route element={<RequireAuth />}>
+            <Route element={<InventoryLayout />}>
                 <Route path='dashboard' element={<Dashboard />} />
             </Route>
-
         </Route>
+
+    </Route>
 ));
 
 const queryClient = new QueryClient({
---
+++ fatog-fe/src/constants/index.jsx
@@ -0,0 +1,9 @@
+import { RxDashboard } from "react-icons/rx";
+import { FaPeopleCarryBox } from "react-icons/fa6";
+import { MdFormatListBulleted } from "react-icons/md";
+
+export const links = [
+    {title: 'Dashboard', ref: '/dashboard', icon: RxDashboard},
+    {title: 'Products', ref: '/products', icon: MdFormatListBulleted},
+    {title: 'Manufacturers', ref: '/manufacturers', icon: FaPeopleCarryBox},
+]
