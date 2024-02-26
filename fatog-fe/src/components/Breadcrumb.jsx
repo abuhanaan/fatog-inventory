ce3c1ced0f1207adbbd9b2e181490a40749ec2a2
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-02-26 22:24:32 +0100

    feat: Create breadcrumb component

---
+++ fatog-fe/src/components/Breadcrumb.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    Breadcrumb as ChakraBreadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+} from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import { MdChevronRight } from "react-icons/md";
+
+const Breadcrumb = ({ linkList }) => {
+    return (
+        <ChakraBreadcrumb spacing='8px' separator={<MdChevronRight color='gray.500' />}>
+            {
+                linkList.map((link, index) => (
+                    <BreadcrumbItem key={index}>
+                        <BreadcrumbLink as={RouterLink} to={link.ref}>{link.name}</BreadcrumbLink>
+                    </BreadcrumbItem>
+                ))
+            }
+        </ChakraBreadcrumb>
+    )
+}
+
+export default Breadcrumb;
