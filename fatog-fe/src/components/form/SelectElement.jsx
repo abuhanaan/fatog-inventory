2a9ea8048a4d1b6a8837e485307d5476282ef9b2
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-02-29 14:44:57 +0100

    fix: Show selected option

--- fatog-fe/src/components/form/SelectElement.jsx
+++ fatog-fe/src/components/form/SelectElement.jsx
@@ -1,13 +1,24 @@
 import { Select, FormControl, FormLabel } from '@chakra-ui/react';
+import { useState } from 'react';
 
-const SelectElement = ({ data, setManufacturerId, label, defaultVal }) => {
+const SelectElement = ({ data, setManufacturerId, label, defaultVal='Select Manufacturer' }) => {
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setSelectedOption(value);
+        setManufacturerId(value);
+    }
+
     return (
         <FormControl>
             <FormLabel htmlFor={label}>{label}</FormLabel>
             <Select
-                value={defaultVal ?? 'Select Manufacturer'}
+                // value={defaultVal ?? 'Select Manufacturer'}
+                value={selectedOption ?? defaultVal}
                 placeholder='Select Manufacturer'
-                onChange={(e) => setManufacturerId(e.target.value)}
+                // onChange={(e) => setManufacturerId(e.target.value)}
+                onChange={handleChange}
             >
                 {
                     data.map((option, index) => (
