8388042c1c2da6ed9c8e0b2f4cc2c3cf499fbcfc
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-03-01 10:56:26 +0100

    feat: Consume get API endpoint for products list

--- fatog-fe/src/pages/products/Products.jsx
+++ fatog-fe/src/pages/products/Products.jsx
@@ -1,13 +1,18 @@
-import { useRef } from 'react';
-import { useNavigate, Link, useLoaderData } from 'react-router-dom';
+import { useRef, useState, useEffect } from 'react';
+import { useNavigate, useNavigation, Link, useLoaderData, useLocation } from 'react-router-dom';
 import ListingsTable from '../../components/Table';
-import { Stack, HStack, Box, useDisclosure, IconButton, Button, Heading, Text } from '@chakra-ui/react';
+import { Stack, HStack, VStack, Box, useDisclosure, IconButton, Icon, Button, Heading, Text, Spinner } from '@chakra-ui/react';
 import { MdOutlineEdit, MdDeleteOutline } from "react-icons/md";
 import { IoEyeOutline } from "react-icons/io5";
+import { BiError } from "react-icons/bi";
+import { FaRegThumbsUp } from "react-icons/fa6";
 import Modal from '../../components/Modal';
 import Breadcrumb from '../../components/Breadcrumb';
 import { EmptySearch } from '../../components/EmptySearch';
 import AddButton from '../../components/AddButton';
+import { getProducts, deleteProduct } from '../../api/products';
+import { useToastHook } from '../../hooks/useToast';
+import { requireAuth } from '../../hooks/useAuth';
 
 const columns = [
     { id: 'S/N', header: 'S/N' },
@@ -20,75 +25,65 @@ const columns = [
     { id: 'actions', header: '' },
 ];
 
-export const products = [
-    {
-        id: 1, name: 'Product 1', type: 'Type 1', weight: 10, size: 100, pricePerBag: 7000, manufacturerId: 1, manufacturer: {
-            id: 1,
-            brandName: "Optimal Feeds",
-            repName: "Ibrahim Salis",
-            repPhoneNumber: "08067565656",
-            createdAt: "2024-02-27T21:29:08.330Z",
-            updatedAt: "2024-02-27T21:29:08.330Z"
-        }, createdAt: "2024-02-27T21:29:08.330Z", updatedAt: "2024-02-27T21:29:08.330Z"
-    },
-    {
-        id: 2, name: 'Product 2', type: 'Type 2', weight: 10, size: 100, pricePerBag: 7000, manufacturerId: 2, manufacturer: {
-            id: 2,
-            brandName: "Vital Feeds",
-            repName: "Asia Yunus",
-            repPhoneNumber: "08012345656",
-            createdAt: "2024-02-27T21:29:08.330Z",
-            updatedAt: "2024-02-27T21:29:08.330Z"
-        }, createdAt: "2024-02-27T21:29:08.330Z", updatedAt: "2024-02-27T21:29:08.330Z"
-    },
-    {
-        id: 3, name: 'Product 3', type: 'Type 3', weight: 10, size: 100, pricePerBag: 7000, manufacturerId: 3, manufacturer: {
-            id: 3,
-            brandName: "KASMAG",
-            repName: "Yewande Smith",
-            repPhoneNumber: "08090876543",
-            createdAt: "2024-02-27T21:29:08.330Z",
-            updatedAt: "2024-02-27T21:29:08.330Z"
-        }, createdAt: "2024-02-27T21:29:08.330Z", updatedAt: "2024-02-27T21:29:08.330Z"
-    },
-];
+export async function loader({ request }) {
+    await requireAuth(request);
+    const response = await getProducts(request);
+
+    return response;
+}
 
 const Products = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [toastState, setToastState] = useToastHook();
+    const products = useLoaderData();
+    const [error, setError] = useState({
+        error: '',
+        message: ''
+    });
     const breadcrumbData = [
         { name: 'Home', ref: '/dashboard' },
         { name: 'Products', ref: '/products' },
     ];
 
-    const productAdd = () => {
-        console.log('Will add product');
-    }
+    useEffect(() => {
+        if (products.error || products.message) {
+            setToastState({
+                title: products.error,
+                description: products.message,
+                status: 'error',
+                icon: <Icon as={BiError} />
+            });
 
-    const drawerButtons =
-        <HStack spacing='3'>
-            <Button colorScheme='red' onClick={onClose}>Cancel</Button>
-            <Button onClick={productAdd} form='product-form' colorScheme='blue'>Submit</Button>
-        </HStack>;
+            setError({
+                error: products.error,
+                message: products.message
+            });
+        }
+    }, []);
 
     return (
-        <Stack spacing='6'>
-            <Box>
-                <Breadcrumb linkList={breadcrumbData} />
-            </Box>
-            <HStack justifyContent='space-between'>
-                <Heading fontSize='3xl' color='blue.700'>Products</Heading>
-                <AddButton navigateTo='create'>Add Product</AddButton>
-            </HStack>
-            <Box marginTop='8'>
-                {
-                    products?.length === 0 ?
-                        <EmptySearch headers={['S/N', 'NAME', 'TYPE', 'WEIGHT', 'SIZE', 'MANUFACTURER']} type='product' /> :
-                        <ListingsTable data={products} columns={columns} fileName='products-data.csv' render={(product) => (
-                            <ActionButtons product={product} />
-                        )} />
-                }
-            </Box>
-        </Stack>
+        error.error ?
+            <VStack>
+                <Box>{error.error}</Box>
+                <Box>{error.message}</Box>
+            </VStack> :
+            <Stack spacing='6'>
+                <Box>
+                    <Breadcrumb linkList={breadcrumbData} />
+                </Box>
+                <HStack justifyContent='space-between'>
+                    <Heading fontSize='3xl' color='blue.700'>Products</Heading>
+                    <AddButton navigateTo='create'>Add Product</AddButton>
+                </HStack>
+                <Box marginTop='8'>
+                    {
+                        products?.length === 0 ?
+                            <EmptySearch headers={['S/N', 'NAME', 'TYPE', 'WEIGHT', 'SIZE', 'MANUFACTURER']} type='product' /> :
+                            <ListingsTable data={products} columns={columns} fileName='products-data.csv' render={(product) => (
+                                <ActionButtons product={product} />
+                            )} />
+                    }
+                </Box>
+            </Stack>
     )
 }
 
@@ -96,6 +91,9 @@ const ActionButtons = ({ product }) => {
     const navigate = useNavigate();
     const { isOpen, onOpen, onClose } = useDisclosure();
     const closeModalRef = useRef(null);
+    const { pathname } = useLocation();
+    const [toastState, setToastState] = useToastHook();
+    const [isDeleting, setIsDeleting] = useState(false);
 
     function viewProduct(e) {
         e.preventDefault();
@@ -107,41 +105,64 @@ const ActionButtons = ({ product }) => {
     async function productDelete(e) {
         e.preventDefault();
 
-        console.log(product.id);
+        setIsDeleting(true);
 
-        // const user = await deleteUser(userId);
+        // TODO: Consume DELETE product endpoint
+        const response = await deleteProduct(product.id);
 
-        // if (user.unAuthorize) {
-        //     const pathname = location.pathname;
-        //     console.log(pathname);
-        //     return redirect(`/?message=Please log in to continue&redirectTo=${pathname}`);
-        // }
+        if (response.unAuthorize) {
+            sessionStorage.removeItem('user');
+            navigate(`/?message=${response.message}. Please log in to continue&redirectTo=${pathname}`);
+        }
 
-        // if (user.error || user.message) {
-        //     toast.error(`${user.error}: ${user.message}`, {
-        //         position: toast.POSITION.TOP_CENTER,
-        //         autoClose: 2000,
-        //     });
-        //     setIsConfirmOpen(false);
-        //     return user.error;
-        // }
+        if (response.error || response.message) {
+            setToastState({
+                title: response.error,
+                description: response.message,
+                status: 'error',
+                icon: <Icon as={BiError} />
+            });
 
-        // toast.success(`User activated successfully!`, {
-        //     position: toast.POSITION.TOP_CENTER,
-        //     autoClose: 2000,
-        // });
+            setIsDeleting(false);
+            closeModalRef.current.click();
+            return response.error;
+        }
 
-        // setTimeout(() => {
-        //     return redirect('/admin/users');
-        // }, 3000);
+        setToastState({
+            title: 'Success!',
+            description: 'Product deleted successfully.',
+            status: 'success',
+            icon: <Icon as={FaRegThumbsUp} />
+        });
 
+        setIsDeleting(false);
         closeModalRef.current.click();
+
+        setTimeout(() => {
+            navigate(`/products`);
+        }, 6000);
+
     }
 
     const modalButtons =
         <HStack spacing='3'>
             <Button colorScheme='red' ref={closeModalRef} onClick={onClose}>Cancel</Button>
-            <Button onClick={productDelete} colorScheme='blue'>Delete</Button>
+            <Button
+                onClick={productDelete}
+                colorScheme='blue'
+                isLoading={isDeleting ? true : false}
+                loadingText='Deleting...'
+                spinnerPlacement='end'
+                spinner={<Spinner
+                    thickness='4px'
+                    speed='0.5s'
+                    emptyColor='gray.200'
+                    color='blue.300'
+                    size='md'
+                />}
+            >
+                Delete
+            </Button>
         </HStack>
 
     return (
