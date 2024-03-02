c2714e1c777212fd162a1c426194799404ebfe1d
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-03-02 09:13:10 +0100

    chore: Remove typescript version of frontend

--- fatog-inventory-FE/src/hooks/useAuth.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import AuthContext from "../context/AuthProvider";
-import { GlobalUserContextType } from "../context/AuthProvider";
-
-const useAuth = () => {
-    return useContext(AuthContext) as GlobalUserContextType;
-};
-
-export default useAuth;
