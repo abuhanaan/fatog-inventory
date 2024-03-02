6061ddcbd8a31f005157b94ce7477460561f5b26
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-03-02 17:16:16 +0100

    chore: Create functions to consume user endpoints

--- fatog-fe/src/api/user.js
+++ fatog-fe/src/api/user.js
@@ -1,7 +1,13 @@
 import { jwtDecode } from 'jwt-decode';
+import { redirect } from 'react-router-dom';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
+const user = JSON.parse(sessionStorage.getItem('user'));
 const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${user?.accessToken}`,
+};
+const authHeaders = {
     'Content-Type': 'application/json'
 };
 
@@ -9,7 +15,7 @@ export const authenticate = async (data) => {
     const body = JSON.stringify(data);
     const method = 'POST';
 
-    const response = await fetch(`${BASE_URL}/auth/login`, { body, method, headers });
+    const response = await fetch(`${BASE_URL}/auth/login`, { body, method, headers: authHeaders });
     const responseData = await response.json();
 
     if (!response.ok) {
@@ -28,16 +34,110 @@ export const authenticate = async (data) => {
         ...responseData,
         user
     }
+};
 
-    // if (data.email === 'admin@gmail.com' && data.password === 'password') {
-    //     return {
-    //         accessToken: 'admin'
-    //     }
-    // }
+export async function createUser(userData) {
+    const res = await fetch(`${BASE_URL}/users`, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(userData)
+    });
 
-    // return {
-    //     error: 'User not found',
-    //     message: 'Invalid login details',
-    //     statusCode: '400'
-    // }
+    const data = await res.json();
+
+    if (res.status === 401) {
+        return {
+            unAuthorized: true,
+            statusCode: data.statusCode,
+            message: data.message,
+            error: data.error ?? 'Unauthorized',
+        }
+    }
+
+    isError(res, data);
+
+    return data;
+}
+
+export async function updateUser(userId, userData) {
+    const res = await fetch(`${BASE_URL}/users/${userId}`, {
+        method: 'PATCH',
+        headers,
+        body: JSON.stringify(userData)
+    });
+
+    const data = await res.json();
+
+    if (res.status === 401) {
+        return {
+            unAuthorized: true,
+            statusCode: data.statusCode,
+            message: data.message,
+            error: data.error ?? 'Unauthorized',
+        }
+    }
+
+    isError(res, data);
+
+    return data;
+}
+
+export async function getUsers(request) {
+    const res = await fetch(`${BASE_URL}/users`, {
+        method: 'GET',
+        headers,
+    });
+
+    const data = await res.json();
+
+    isUnauthorized(res, request);
+    isError(res, data);
+
+    return data;
+}
+
+export async function getUser(request, userId) {
+    const res = await fetch(`${BASE_URL}/users/${userId}`, {
+        method: 'GET',
+        headers,
+    });
+
+    const data = await res.json();
+
+    isUnauthorized(res, request);
+    isError(res, data);
+
+    return data;
+}
+
+export async function deleteUser(request, userId) {
+    const res = await fetch(`${BASE_URL}/users/${userId}`, {
+        method: 'DELETE',
+        headers,
+    });
+
+    const data = await res.json();
+
+    isUnauthorized(res, request);
+    isError(res, data);
+
+    return data;
+}
+
+const isUnauthorized = (res, request) => {
+    if (res.status === 401) {
+        const pathname = new URL(request.url).pathname;
+        throw redirect(`/?message=Please log in to continue&redirectTo=${pathname}`);
+    }
+};
+
+const isError = (res, data) => {
+    if (!res.ok || data.error) {
+        return {
+            statusCode: data.statusCode,
+            message: data.message,
+            error: data.error ?? 'Something went wrong',
+            path: data.path
+        }
+    }
 };
