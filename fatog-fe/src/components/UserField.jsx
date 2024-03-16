66ce321d699af68d14c4d050a3e28b6cfd4a6830
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2024-03-16 11:32:00 +0100

    refactor: Split field names & render monetray value

--- fatog-fe/src/components/UserField.jsx
+++ fatog-fe/src/components/UserField.jsx
@@ -1,10 +1,21 @@
-import {Box, Heading, Text} from '@chakra-ui/react';
+import { Box, Heading, Text } from '@chakra-ui/react';
+import { getMonetaryValue } from '../pages/stocks/StockCreate';
+
+export const splitFieldName = (str) => {
+    return str.split(/(?=[A-Z])/).join(' ');
+}
 
 const UserField = ({ field }) => {
     return (
         <Box as='fieldset' boxSize={{ base: 'full', lg: 'full' }} p='3' borderWidth='1px' borderColor='gray.300' borderRadius='md'>
-            <Heading as='legend' px='1' fontSize='sm' fontWeight='semibold' textTransform='capitalize'>{field.key}</Heading>
-            <Text>{field.value ? field.value : 'N/A'}</Text>
+            <Heading as='legend' px='1' fontSize='sm' fontWeight='semibold' textTransform='capitalize'>{splitFieldName(field.key)}</Heading>
+            <Text>
+                {
+                    (field.key === 'totalAmount' || field.key === 'pricePerBag') && (field.value) ?
+                    getMonetaryValue(field.value) : field.value ? 
+                    field.value : 'N/A'
+                }
+            </Text>
         </Box>
     )
 }
